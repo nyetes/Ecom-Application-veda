de52b8df54b985736680e8e75caa63739176e302
nyetes <nitesh.mahar1208@gmail.com>
2024-06-27 22:49:19 +0545

    config routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import productsRouter from "./routes/products";
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+
+//routes
+app.use("/api", productsRouter);
 
 app.listen(PORT, () =>
   console.log(`Server running on port http://localhost:${PORT}`)

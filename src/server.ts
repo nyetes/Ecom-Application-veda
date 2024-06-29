0dfba27a0e8964f292ad22234ad01794ad0d52ba
nyetes <nitesh.mahar1208@gmail.com>
2024-06-29 20:38:46 +0545

    Refactor: Simplify AdminJS setup and imports

--- src/server.ts
+++ src/server.ts
@@ -1,58 +1,45 @@
 import express from "express";
 import bodyParser from "body-parser";
+import AdminJS from "adminjs";
+import AdminJSExpress from "@adminjs/express";
 import { PrismaClient } from "@prisma/client";
+import { Database, Resource } from "@adminjs/prisma";
 
 import productsRouter from "./routes/products";
 
 const prisma = new PrismaClient();
+AdminJS.registerAdapter({ Database, Resource });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-let AdminJS;
-let AdminJSExpress;
-let Database;
-let Resource;
-
-Promise.all([
-  import("adminjs"),
-  import("@adminjs/express"),
-  import("@adminjs/prisma"),
-]).then(([adminjs, adminjsExpress, adminjsPrisma]) => {
-  AdminJS = adminjs.default;
-  AdminJSExpress = adminjsExpress.default;
-  Database = adminjsPrisma.Database;
-  Resource = adminjsPrisma.Resource;
-
-  AdminJS.registerAdapter({ Database, Resource });
-
-  const adminJs = new AdminJS({
-    resources: [
-      {
-        resource: { model: prisma.product },
-        options: {
-          listProperties: ["image", "title", "category", "price", "rating"],
-        },
+const adminJs = new AdminJS({
+  resources: [
+    {
+      resource: { model: prisma.product },
+      options: {
+        listProperties: ["image", "title", "category", "price", "rating"],
       },
-      {
-        resource: { model: prisma.category },
-        options: {
-          listProperties: ["name"],
-        },
+    },
+    {
+      resource: { model: prisma.category },
+      options: {
+        listProperties: ["name"],
       },
-    ],
-    rootPath: "/admin",
-  });
-
-  app.use(bodyParser.json());
-
-  // routes
-  app.use("/api", productsRouter);
-
-  const adminRouter = AdminJSExpress.buildRouter(adminJs);
-  app.use(adminJs.options.rootPath, adminRouter);
-
-  app.listen(PORT, () =>
-    console.log(`Server running on port http://localhost:${PORT}`)
-  );
+    },
+  ],
+  rootPath: "/admin",
 });
+
+app.use(express.json()); // replaced bodyParser.json() with express.json()
+app.use(express.urlencoded({ extended: true })); // added to support URL-encoded bodies
+
+// routes
+app.use("/api", productsRouter);
+
+const adminRouter = AdminJSExpress.buildRouter(adminJs);
+app.use(adminJs.options.rootPath, adminRouter);
+
+app.listen(PORT, () =>
+  console.log(`Server running on port http://localhost:${PORT}`)
+);
